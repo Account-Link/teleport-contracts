b858895992130cf28d9aa431cd468445e1128bf4
Roshan <19766713+rpalakkal@users.noreply.github.com>
2024-06-28 13:39:15 -0400

    feat: initial commit

---
+++ script/NFT.s.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/NFT.sol";
+
+contract MyScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        NFT nft = new NFT(
+            "Teleport",
+            "TPN",
+            "test",
+            address(0x36e7Fda8CC503D5Ec7729A42eb86EF02Af315Bf9)
+        );
+
+        vm.stopBroadcast();
+    }
+}
---
+++ src/NFT.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, Ownable {
+    using Strings for uint256;
+
+    error NonExistentTokenURI();
+
+    enum TokenType {
+        TWEET,
+        LIKE
+    }
+
+    struct TokenData {
+        uint256 x_id;
+        string policy;
+    }
+
+    // Mapping from token ID to its data
+    mapping(uint256 => TokenData) private tokenDataMap;
+
+    // Event emitted on top of `ERC20::Transfer` for granularity & indexer data availability
+    event NewTokenData(
+        uint256 indexed tokenId,
+        uint256 indexed x_id,
+        address to,
+        string policy
+    );
+    // Event to be emitted upon redemption of a tweet
+    event RedeemTweet(
+        uint256 indexed tokenId,
+        uint256 indexed x_id,
+        string policy,
+        string content
+    );
+    // Event to be emitted upon redemption of a tweet like
+    event RedeemLike(
+        uint256 indexed tokenId,
+        uint256 indexed x_id,
+        string policy,
+        string tweetId
+    );
+
+    string public baseURI;
+    uint256 public currentTokenId;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseURI,
+        address initialOwner
+    ) ERC721(_name, _symbol) Ownable(initialOwner) {
+        baseURI = _baseURI;
+    }
+
+    function mintTo(
+        address recipient,
+        uint256 x_id,
+        string memory policy
+    ) public onlyOwner returns (uint256) {
+        uint256 newTokenId = ++currentTokenId;
+        _safeMint(recipient, newTokenId);
+
+        tokenDataMap[newTokenId] = TokenData(x_id, policy);
+
+        emit NewTokenData(newTokenId, x_id, recipient, policy);
+
+        return newTokenId;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        if (ownerOf(tokenId) == address(0)) {
+            revert NonExistentTokenURI();
+        }
+        return
+            bytes(baseURI).length > 0
+                ? string.concat(baseURI, tokenId.toString())
+                : "";
+    }
+
+    // Redeem function that emits an event
+    function redeem(
+        uint256 tokenId,
+        string memory content,
+        TokenType tokenType
+    ) public {
+        require(
+            ownerOf(tokenId) == msg.sender || msg.sender == owner(),
+            "Caller is not the token owner or the contract owner"
+        );
+
+        TokenData memory data = tokenDataMap[tokenId];
+
+        // emit RedeemTweet(tokenId, data.x_id, data.policy, content);
+        if (tokenType == TokenType.TWEET) {
+            emit RedeemTweet(tokenId, data.x_id, data.policy, content);
+        } else if (tokenType == TokenType.LIKE) {
+            emit RedeemLike(tokenId, data.x_id, data.policy, content);
+        } else {
+            revert("Invalid token type");
+        }
+
+        // burn NFT
+        _burn(tokenId);
+    }
+}
