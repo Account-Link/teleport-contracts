48a4fa8ec2bebb2960239d2ec92c195d69046e18
Roshan <19766713+rpalakkal@users.noreply.github.com>
2024-09-25 10:35:49 -0400

    chore: new deploy

--- script/NFT.s.sol
+++ script/NFT.s.sol
@@ -6,14 +6,17 @@ import "../src/NFT.sol";
 contract MyScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address minter = 0x36e7Fda8CC503D5Ec7729A42eb86EF02Af315Bf9;
         vm.startBroadcast(deployerPrivateKey);
 
         NFT nft = new NFT(
             "Account.link",
             "account.link",
             "https://account.link/metadata/",
-            address(0x36e7Fda8CC503D5Ec7729A42eb86EF02Af315Bf9)
+            minter
         );
+
+        nft.whitelistMinter(minter);
 
         vm.stopBroadcast();
     }
